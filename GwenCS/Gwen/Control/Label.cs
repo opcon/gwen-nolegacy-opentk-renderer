84574aaf059c5c4df003b92979f0c56dfe5c0070
Patrick Yates <frozen.dinosaur@gmail.com>
2015-06-25 17:13:13 +1000

    Invalidate text if label is invalidated

--- GwenCS/Gwen/Control/Label.cs
+++ GwenCS/Gwen/Control/Label.cs
@@ -251,5 +251,11 @@ namespace Gwen.Control
         protected override void Render(Skin.Base skin)
         {
         }
+
+        public override void Invalidate()
+        {
+            if (m_Text != null) m_Text.Invalidate();
+            base.Invalidate();
+        }
     }
 }
